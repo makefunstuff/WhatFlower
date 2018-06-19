422a30029b9026fa855b72e1f1a95bf49c9d4c99
Iurii Plugatarov <plugatariov@gmail.com>
2018-06-19 23:02:11 +0300

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+## WhatFlower
+
+Playing a bit more with CoreML.
+Now app uses pretrained caffe model converted to mlmodel.
+
+## Usage
+Make photo of any flower then you will see flower name and flower description fetched from wikipedia
+
+## Tools Used
+
+* CoreML
+* Alamofire
+* SwiftyJSON
--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  let imagePicker = UIImagePickerController()
+  @IBOutlet weak var imageView: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    imagePicker.delegate = self
+    imagePicker.allowsEditing = false
+    imagePicker.sourceType = .camera
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+    
+    guard let visionImage = CIImage(image: userPickedImage!) else { fatalError("Cannot convert image") }
+    
+    detect(image: visionImage)
+    
+    imageView.image = userPickedImage
+    imagePicker.dismiss(animated: true, completion: nil)
   }
 
 
+  @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    present(imagePicker, animated: true, completion: nil)
+  }
+  
+  func detect(image: CIImage) {
+    guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {fatalError("Could not import model")}
+    
+    let request = VNCoreMLRequest(model: model) { request, error in
+      let classification = request.results?.first as? VNClassificationObservation
+      
+      self.navigationItem.title = classification?.identifier.capitalized
+    }
+    let handler = VNImageRequestHandler(ciImage: image)
+    
+    do {
+      try handler.perform([request])
+    } catch {
+      print("could not process mlmodel")
+    }
+  }
 }
 
